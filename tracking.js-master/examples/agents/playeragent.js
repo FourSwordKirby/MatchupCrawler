acf15b55788852622ff64468f5b4f50e224cd877
Xecese <chrisyliu426@gmail.com>
2015-02-07 00:59:18 -0500

    Adding getReadings

--- tracking.js-master/examples/agents/playeragent.js
+++ tracking.js-master/examples/agents/playeragent.js
@@ -9,6 +9,13 @@ function Player(type){
     this.timeStamps = []
 }
 
+function Readings(type){
+    this.states = [];
+    this.x = [];
+    this.y = [];
+    this.time = [];
+}
+
 function playerUpdatePosition(player, rect, state){
     player.xPositions[player.xPositions.length] = rect.x;
     player.yPositions[player.yPositions.length] = rect.y;
@@ -21,13 +28,28 @@ function playerUpdatePosition(player, rect, state){
         player.yPositions.splice(0, 1);
         player.timeStamps.splice(0, 1);
         player.states.splice(0, 1);
-    }
-    
-    
+    }   
 }
 
 function checkColorInBounds(r1, g1, b1, r2, g2, b2, leniency){
     return (Math.abs(r1 - r2) < leniency && Math.abs(g1 - g2) < leniency && Math.abs(b1 - b2) < leniency);
+}
+
+function getReadings(player, n){
+    var arrayLength = player.states.length
+    if (arrayLength < n){
+        n = arrayLength;
+    }
+    var readings = [];
+    for (var i = 0; i < n; i++){
+        var newReading = new Reading;
+        newReading.state = player.states[arrayLength - n + i]
+        newReading.x = player.xPositions[arrayLength - n + i]
+        newReading.y = player.yPositions[arrayLength - n + i]
+        newReading.time = player.timeStamps[arrayLength - n + i]
+        readings[i] = newReading;
+    }
+    return readings;
 }
 
 function determinePlayer(rect){//if needed
