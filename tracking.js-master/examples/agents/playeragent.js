7197508e86fcee45d8c566332f772623814566be
Xecese <chrisyliu426@gmail.com>
2015-02-06 23:56:06 -0500

    added checkColorInBounds and timestamps

--- tracking.js-master/examples/agents/playeragent.js
+++ tracking.js-master/examples/agents/playeragent.js
@@ -6,19 +6,26 @@ function Player(type){
     this.states = [];
     this.xPositions = [];
     this.yPositions = [];
+    this.timeStamps = []
 }
 
 function playerUpdatePosition(player, rect){
     player.xPositions[player.xPositions.length] = rect.x;
     player.yPositions[player.yPositions.length] = rect.y;
+    var d = new Date();
+    var time = d.getTime();
+    player.timeStamps[player.timeStamps.length] = time;
     if (player.xPositions.length > positionMaxLength){
         player.xPositions.splice(0, 1);
         player.yPositions.splice(0, 1);
+        player.timeStamps.splice(0, 1);
     }
+    
+    
 }
 
 function checkColorInBounds(r1, g1, b1, r2, g2, b2, leniency){
-    return (Math.abs(r1 - r2) < leniency && math.abs(g1 - g2) < leniency && Math.abs(b1 - b2) < leniency);
+    return (Math.abs(r1 - r2) < leniency && Math.abs(g1 - g2) < leniency && Math.abs(b1 - b2) < leniency);
 }
 
 function determinePlayer(rect){
