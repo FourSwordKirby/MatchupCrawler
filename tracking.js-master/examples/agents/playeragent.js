5033ce7d3f78d3bc3f9a5cb527b18534c4708584
Xecese <chrisyliu426@gmail.com>
2015-02-07 00:38:00 -0500

    added in events for states

--- tracking.js-master/examples/agents/playeragent.js
+++ tracking.js-master/examples/agents/playeragent.js
@@ -1,5 +1,5 @@
 var players = [];
-var positionMaxLength = 100;
+var positionMaxLength = 50;
 
 function Player(type){
     this.name = "";
@@ -9,16 +9,18 @@ function Player(type){
     this.timeStamps = []
 }
 
-function playerUpdatePosition(player, rect){
+function playerUpdatePosition(player, rect, state){
     player.xPositions[player.xPositions.length] = rect.x;
     player.yPositions[player.yPositions.length] = rect.y;
-    //var d = new Date();
-    //var time = d.getTime();
-    //player.timeStamps[player.timeStamps.length] = time;
+    var d = new Date();
+    var time = d.getTime();
+    player.timeStamps[player.timeStamps.length] = time;
+    player.states[player.states.length] = state
     if (player.xPositions.length > positionMaxLength){
         player.xPositions.splice(0, 1);
         player.yPositions.splice(0, 1);
         player.timeStamps.splice(0, 1);
+        player.states.splice(0, 1);
     }
     
     
@@ -28,8 +30,8 @@ function checkColorInBounds(r1, g1, b1, r2, g2, b2, leniency){
     return (Math.abs(r1 - r2) < leniency && Math.abs(g1 - g2) < leniency && Math.abs(b1 - b2) < leniency);
 }
 
-function determinePlayer(rect){
+function determinePlayer(rect){//if needed
     for (var i = 0; i < players.length; i++){
-        
+        return;
     }
 }
